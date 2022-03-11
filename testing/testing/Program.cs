cc59c6d44fb06b530968861bbd75f5e339445b6f
Chubankem <101402272+Chubankem@users.noreply.github.com>
2022-03-11 19:41:03 +0700

    abcd
    1234

---
+++ testing/testing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    internal class Program
+    {
+        static void xuat()
+        {
+            Console.WriteLine("tan ngu nhu cho");
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                xuat();
+            }
+        }
+    }
+}
